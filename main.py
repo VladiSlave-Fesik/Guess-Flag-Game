eaacd03a381dfcb5d95481b89ee5aa2b0368fe63
Vladislav Fesik <vladfesik36@gmail.com>
2022-11-19 21:41:44 +0200

    Add commands for players
    Including open() that show original flag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 try:
     from iso3166 import countries as cnt
-    from random import choice,shuffle
+    from random import choice, shuffle
     import pyfiglet as fg
     import os
     from PIL import Image
@@ -10,7 +10,6 @@ try:
 except ImportError as error:
     print(error)
     input('Press Enter to close game\n')
-
 
 init_colorit()
 
@@ -23,7 +22,6 @@ lives = 3
 
 
 def print_flag(img: str, folder: str = flags_folder, depth: int = 20):
-
     img = Image.open(folder + img)
     img = img.resize((depth, depth))
 
@@ -33,12 +31,40 @@ def print_flag(img: str, folder: str = flags_folder, depth: int = 20):
         print()
 
 
-def round():
-    global lives,scores,num_rounds
+def open_flag(img_name: str, folder='iso3166\\'):
+    img = Image.open(folder + img_name)
+
+    img.save('a.png')
+
+    img2 = Image.open('a.png')
+    img2 = img2.convert('RGB')
+    img2.show()
+
+
+def del_a():
+    os.remove('a.png')
+
+
+def inf_lives():
+    global lives
+    lives = 2 ** 20
+
+
+def help_():
+    print('''
+    List of all commands:
+    help - to see all commands
+    inf_lives - you will never lose
+    open - open a original photo of flag
+    ''')
+
+
+def round_():
+    global lives, scores, num_rounds
 
     print(f'Round number {num_rounds}\nLives {lives}\nScores {scores}\n')
 
-    print_flag(right_flag:= choice(flags))
+    print_flag(right_flag := choice(flags))
 
     answers_list = []
 
@@ -56,9 +82,28 @@ def round():
 
     answer = input('\nInput a code: ')
 
+    while answer not in answers_list and answer not in commands:
+        answer = input('You type incorrect code.\nInput a code: ')
+
+    if answer in commands:
+        if answer == 'open':
+            open_flag(right_flag)
+            del_a()
+            answer = input('\nInput a code: ')
+            while answer not in answers_list:
+                answer = input('You type incorrect code.\nInput a code: ')
+
+        else:
+            commands[answer]()
+
+            answer = input('\nInput a code: ')
+            while answer not in answers_list:
+                answer = input('You type incorrect code.\nInput a code: ')
+
     if answer != right_flag.replace('.png', ''):
         lives -= 1
-        print(f'Wrong! Right answer - {cnt.get(right_flag.replace(".png","") ).name}|{right_flag.replace(".png","")}\n')
+        print(
+            f'Wrong! Right answer - {cnt.get(right_flag.replace(".png", "")).name}|{right_flag.replace(".png", "")}\n')
 
 
     else:
@@ -72,15 +117,25 @@ def round():
 def main():
     hello_world = fg.Figlet(font='slant', width=500).renderText('Q u e s s   F l a g')
     print(hello_world)
-    print('In this game you have to guess which country is depicted on the minimalist flag')
-    input('Press Enter to start game\n')
+    print('In this game you have to guess which country is depicted on the minimalist flag. If you want to know all '
+          'commands type help')
+    ans = input('Press Enter to start game\n')
 
-    while lives != 0:
-        round()
+    if ans == 'help':
+        help_()
+
+    while lives != 0 and scores != 250:
+        round_()
+
+    if scores == 250:
+        win = fg.Figlet(font='slant', width=500).renderText('You Win!')
+        print(win)
 
     print(f'Your result:\n Scores - {scores} \n Number of rounds - {num_rounds}')
 
+
+commands = {'help': help_, 'inf_lives': inf_lives, 'open': open_flag}
+
 if __name__ == '__main__':
     main()
-
-input('Press Enter to close game\n')
+    input('Press Enter to close game\n')
