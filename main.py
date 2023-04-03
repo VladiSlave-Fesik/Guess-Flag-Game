e792103973a28365e698b8d3d7bdfae6b27131ff
VladiSlave-Fesik <vladfesik36@gmail.com>
2023-04-03 13:20:36 +0300

    Reworked the code, added new commands
    Changed the retrieval of the name of the country, now it's all written in a text file, which (if one wants) can create a custom version of the game by adding their own countries or removing existing ones.
    Moved the code from colorit inside the code (first wrote his own, but it turned out the same)
    Removed iso3166, pyfiglet (the message I just copied and pasted inside) , colorit
    Made a new executable

--- main.py
+++ main.py
@@ -1,43 +1,115 @@
 try:
-    from iso3166 import countries as cnt
     from random import choice, shuffle
-    import pyfiglet as fg
     import os
+    import sys
     from PIL import Image
-    from colorit import background, init_colorit
     from time import sleep, time
 
 except ImportError as error:
     print(error)
     input('Press Enter to close game\n')
 
-init_colorit()
+with open('iso3166_names.txt', 'r') as f:
+    lines = f.readlines()
+
+countries = {}
+
+for line in lines:
+    code, name = line.strip().split('|')
+    countries[code] = name
 
 flags_folder = 'iso3166_resized\\'
 flags = os.listdir(flags_folder)
+len_flags = len(flags)
+
+hello_world = '''
+   ____                                           ______   __                 
+  / __ \    __  __   ___     _____   _____       / ____/  / /  ____ _   ____ _
+ / / / /   / / / /  / _ \   / ___/  / ___/      / /_     / /  / __ `/  / __ `/
+/ /_/ /   / /_/ /  /  __/  (__  )  (__  )      / __/    / /  / /_/ /  / /_/ / 
+\___\_\   \__,_/   \___/  /____/  /____/      /_/      /_/   \__,_/   \__, /  
+                                                                     /____/   
+'''
+
+mega_win = '''
+__  __               __  __________________       _       ___       __
+\ \/ /___  __  __   /  |/  / ____/ ____/   |     | |     / (_)___  / /
+ \  / __ \/ / / /  / /|_/ / __/ / / __/ /| |     | | /| / / / __ \/ / 
+ / / /_/ / /_/ /  / /  / / /___/ /_/ / ___ |     | |/ |/ / / / / /_/  
+/_/\____/\__,_/  /_/  /_/_____/\____/_/  |_|     |__/|__/_/_/ /_(_)   
+'''
+
+win = '''
+__  __               _       ___       __
+\ \/ /___  __  __   | |     / (_)___  / /
+ \  / __ \/ / / /   | | /| / / / __ \/ / 
+ / / /_/ / /_/ /    | |/ |/ / / / / /_/  
+/_/\____/\__,_/     |__/|__/_/_/ /_(_)  
+'''
+
+lose = '''
+    __                            __
+   / /   ____  ________       _ _/_/
+  / /   / __ \/ ___/ _ \     (_) /  
+ / /___/ /_/ (__  )  __/    _ / /   
+/_____/\____/____/\___/    (_) /    
+                             |_|  
+'''
 
 scores = 0
 num_rounds = 0
 lives = 3
 
 
+def init_colorit():
+    if sys.platform.startswith("win32"):
+        os.system("cls")
+    elif sys.platform.startswith("darwin") or sys.platform.startswith("linux"):
+        os.system("clear")
+
+
+def color(text, rgb):
+    return "\033[38;2;{};{};{}m{}\033[0m".format(
+        str(rgb[0]), str(rgb[1]), str(rgb[2]), text
+    )
+
+
+def color_ansi(text, color):
+    return "{}{}\033[0m".format(color, text)
+
+
+def background(text, rgb):
+    return "\033[48;2;{};{};{}m{}\033[0m".format(
+        str(rgb[0]), str(rgb[1]), str(rgb[2]), text
+    )
+
+
 def print_flag(img: str, folder: str = flags_folder, depth: int = 20):
+    global output
+
     img = Image.open(folder + img)
     img = img.resize((depth, depth))
-
+    o = ''
     for y in range(img.size[1]):
         for x in range(img.size[0]):
-            print(background('    ', img.getpixel((x, y))), end='')
-        print()
+            o += background('    ', img.getpixel((x, y)))
+        o += '\n'
+    print(o)
+    output += o
 
 
-def open_flag(img_name: str, folder:str='iso3166\\'):
-    img = Image.open(folder + img_name)
+def open_flag(folder: str = 'iso3166\\'):
+    global right_flag
+
+    try:
+        img = Image.open(folder + right_flag)
+    except:
+        img = Image.open('iso3166_resized\\' + right_flag)
 
     img.save('a.png')
 
     img2 = Image.open('a.png')
-    img2 = img2.convert('RGB')
+    img2 = img2.convert('RGBA')
     img2.show()
 
 
@@ -56,16 +128,30 @@ def help_():
     help - to see all commands
     inf_lives - you will never lose
     open - open a original photo of flag
+    reset - overwrites the output in the console, needed when switching the console full screen mode
+    exit - exit 
     ''')
 
 
-def round_():
-    global lives, scores, num_rounds
+def exit_():
+    del_a()
+    exit()
 
-    print(f'Round number {num_rounds}\nLives {lives}\nScores {scores}\n')
+
+def reset():
+    os.system('cls')
+    print(output)
+
+
+def round_():
+    global lives, scores, num_rounds, right_flag, output
+    output = ''
+
+    o = f'Round number {num_rounds}\nLives {lives}\nScores {scores}\n'
+    print(o)
+    output += o
 
     print_flag(right_flag := choice(flags))
-
     answers_list = []
 
     while len(answers_list) != 3:
@@ -76,40 +162,30 @@ def round_():
 
     shuffle(answers_list)
 
+    o = ''
     for code in answers_list:
-        country_name = cnt.get(code).name
-        print(f'{country_name}|{code}')
+        country_name = countries.get(code)
+        o += f'{country_name}|{code}\n'
+    print(o)
+    output += o
 
-    answer = input('\nInput a code: ')
+    answer = input('\nInput a code: ').lower()
 
-    while answer not in answers_list and answer not in commands:
-        answer = input('You type incorrect code.\nInput a code: ')
-
-    if answer in commands:
-        if answer == 'open':
-            open_flag(right_flag)
-            del_a()
-            answer = input('\nInput a code: ')
-            while answer not in answers_list:
-                answer = input('You type incorrect code.\nInput a code: ')
-
+    while answer not in answers_list:
+        if answer in commands:
+            commands[answer.lower()]()
+            answer = input('Input a code: ').lower()
         else:
-            commands[answer]()
-
-            answer = input('\nInput a code: ')
-            while answer not in answers_list:
-                answer = input('You type incorrect code.\nInput a code: ')
+            answer = input('You type incorrect code.\nInput a code: ').lower()
 
     if answer != right_flag.replace('.png', ''):
         lives -= 1
         print(
-            f'Wrong! Right answer - {cnt.get(right_flag.replace(".png", "")).name}|{right_flag.replace(".png", "")}\n')
-
+            f'Wrong! Right answer - {countries.get(right_flag.replace(".png", ""))}|{right_flag.replace(".png", "")}\n')
 
     else:
         scores += 1
         print('Correct!\n')
-
 
     flags.remove(right_flag)
 
@@ -118,7 +194,8 @@ def round_():
 
 
 def main():
-    hello_world = fg.Figlet(font='slant', width=500).renderText('Q u e s s   F l a g')
+    init_colorit()
+
     print(hello_world)
     print('In this game you have to guess which country is depicted on the minimalist flag. If you want to know all '
           'commands type help')
@@ -127,21 +204,22 @@ def main():
     if ans == 'help':
         help_()
 
-    while lives != 0 and scores != 250 and len(flags) != 0:
+    while True:
         round_()
-
-    if scores == 250:
-        win = fg.Figlet(font='slant', width=500).renderText('You MEGA Win!')
-        print(win)
-
-    elif len(flags) == 0:
-        win = fg.Figlet(font='slant', width=500).renderText('You Win!')
-        print(win)
-
+        if scores == len_flags:
+            res = mega_win
+            break
+        elif len(flags) == 0:
+            res = win
+            break
+        elif lives == 0:
+            res = lose
+            break
+    print(res)
     print(f'Your result:\n Scores - {scores} \n Number of rounds - {num_rounds}')
 
 
-commands = {'help': help_, 'inf_lives': inf_lives, 'open': open_flag}
+commands = {'help': help_, 'inf_lives': inf_lives, 'open': open_flag, 'exit': exit_, 'reset': reset}
 
 if __name__ == '__main__':
     main()
